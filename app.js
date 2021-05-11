0cc63d0f8c66832657d87642beeba1d8a435169f
deyaa1997 <deyah.mosa@gmail.com>
2021-05-11 13:09:30 +0300

    pulse check 3

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ const port = 3000;
 
 app.use(express.json());
 
+app.get("/" , (req,res) => {
+    const readFile = () => {
+        fs.readFile("./data.txt", (err, data) => {
+            if (err) throw err;
+            content = data.toString();
+            console.log(content);
+          });
+      };
+      readFile()
+
+      const writeFile = () => {
+        fs.writeFile("text.txt", "Hello World", (err) => {
+            if (err) throw err;
+            console.log("The file has been saved");
+          });
+      };
+      writeFile();
+
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
   });
