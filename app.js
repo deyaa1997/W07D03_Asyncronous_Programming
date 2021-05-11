8ead2ffb6d8c01a6df56dcc8070e2393eced871d
deyaa1997 <deyah.mosa@gmail.com>
2021-05-11 13:33:05 +0300

    finish all pulse check

--- app.js
+++ app.js
@@ -1,48 +1,61 @@
 const fs = require("fs");
 const axios = require("axios");
-const express = require("express")
+const express = require("express");
+const { kMaxLength } = require("buffer");
 
-const app = express()
+const app = express();
 
 const port = 3000;
 
 app.use(express.json());
 
-app.get("/" , (req,res) => {
-    const readFile = () => {
-        fs.readFile("./data.txt", (err, data) => {
-            if (err) throw err;
-            content = data.toString();
-            console.log(content);
-          });
-      };
-      readFile()
+app.get("/", (req, res) => {
+  const readFile = () => {
+    fs.readFile("./data.txt", (err, data) => {
+      if (err) throw err;
+      content = data.toString();
+      console.log(content);
+    });
+  };
+  readFile();
 
-      const writeFile = () => {
-        fs.writeFile("text.txt", "Hello World", (err) => {
-            if (err) throw err;
-            console.log("The file has been saved");
-          });
-      };
-      writeFile();
+  const writeFile = () => {
+    fs.writeFile("text.txt", "Hello World", (err) => {
+      if (err) throw err;
+      console.log("The file has been saved");
+    });
+  };
+  writeFile();
 
-      const getPost = (id) => {
-        axios.get("https://jsonplaceholder.typicode.com/posts/" + id)
-        .then((response) => {
-            console.log(response.data);
-          })
-          // in `.catch()` we add the code to handel the error
-          .catch((err) => {
-            throw err;
-          });
-      };
-      
-      getPost(1);
-      getPost(50);
+  const getPost = (id) => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts/" + id)
+      .then((response) => {
+        console.log(response.data);
+      })
+      // in `.catch()` we add the code to handel the error
+      .catch((err) => {
+        throw err;
+      });
+  };
 
-})
+  getPost(1);
+  getPost(50);
 
+  const getPostAsync = async (data) => {
+    try {
+      const response = await axios.get(
+        "https://jsonplaceholder.typicode.com/posts/" + data
+      );
+      console.log(response.data);
+    } catch (err) {
+      throw err;
+    }
+  };
+  getPostAsync(1);
+  getPostAsync(50);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-  });
+  console.log(`Example app listening at http://localhost:${port}`);
+});
